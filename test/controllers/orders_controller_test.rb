562d9f0fbd8e0a95dfa7659caed1092865800959
Joseph Kalule <joseph.savio.kalule@gmail.com>
2019-03-07 11:10:45 +0000

    Add the basic tests

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @order = orders(:one)
+    @table = tables(:one)
   end
 
   test "should get index" do
@@ -11,38 +12,38 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_order_url
+    get new_table_order_path(@table)
     assert_response :success
   end
 
   test "should create order" do
     assert_difference('Order.count') do
-      post orders_url, params: { order: { is_paid: @order.is_paid, table_id_id: @order.table_id_id } }
+      post table_orders_url(@table), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
     end
 
-    assert_redirected_to order_url(Order.last)
+    assert_redirected_to table_order_path(Order.last.table, Order.last)
   end
 
   test "should show order" do
-    get order_url(@order)
+    get table_order_path(@order.table_id, @order)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_order_url(@order)
+    get edit_table_order_path(@order.table, @order)
     assert_response :success
   end
 
   test "should update order" do
-    patch order_url(@order), params: { order: { is_paid: @order.is_paid, table_id_id: @order.table_id_id } }
-    assert_redirected_to order_url(@order)
+    patch table_order_path(@order.table, @order), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
+    assert_redirected_to table_order_path(@order.table, @order)
   end
 
   test "should destroy order" do
     assert_difference('Order.count', -1) do
-      delete order_url(@order)
+      delete table_order_path(@order.table_id, @order)
     end
 
-    assert_redirected_to orders_url
+    assert_redirected_to table_orders_path(@order.table_id)
   end
 end
