29da71b8c0d8c28474d704afa57a6a326cf5c4f1
Joseph Kalule <joseph.savio.kalule@gmail.com>
2019-03-11 13:24:57 +0000

    Use shallow nesting

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -18,30 +18,30 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create order" do
     assert_difference('Order.count') do
-      post table_orders_url(@table), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
+      post table_orders_path(@table), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
     end
 
-    assert_redirected_to table_order_path(Order.last.table, Order.last)
+    assert_redirected_to order_path(Order.last)
   end
 
   test "should show order" do
-    get table_order_path(@order.table_id, @order)
+    get order_path(@order)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_table_order_path(@order.table, @order)
+    get edit_order_path(@order)
     assert_response :success
   end
 
   test "should update order" do
-    patch table_order_path(@order.table, @order), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
-    assert_redirected_to table_order_path(@order.table, @order)
+    patch order_path(@order), params: { order: { is_paid: @order.is_paid, table_id: @order.table_id } }
+    assert_redirected_to order_path(@order)
   end
 
   test "should destroy order" do
     assert_difference('Order.count', -1) do
-      delete table_order_path(@order.table_id, @order)
+      delete order_path(@order)
     end
 
     assert_redirected_to table_orders_path(@order.table_id)
